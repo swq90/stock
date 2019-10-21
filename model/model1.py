1f8e2b58e4009ddf5a42bcfb92625a08ddd9d43a
Jane <shaowenqin620@163.com>
2019-10-21 23:15:02 +0800

    model

--- model/model1.py
+++ model/model1.py
@@ -1,12 +1,13 @@
 # 某股票需要满足：在p1时间段内，超过p2%的交易日*均价上涨，且close>open,满足(high-close)/(close-open)>p3
 # 1.所有股票代码 。2.
+# 10天内8次上涨，下引线比例大于0.3
 
 import pandas as pd
 import time
 import datetime
 import data.get_data as gd
 
-def f1(start_date = '20190920',end_date="20191018", period=15, times=12, pct=0.8):
+def f1(start_date = '20190928',end_date="20191021", period=10, times=8, pct=0.3):
     stocks = []
     # 获取股票代码list
     # if not end_date:
@@ -17,21 +18,32 @@ def f1(start_date = '20190920',end_date="20191018", period=15, times=12, pct=0.8
     # print(end_date)
     #['ts_code', 'trade_date', 'open', 'high', 'low', 'close', 'pre_close', 'change', 'pct_chg', 'vol', 'amount']
     # 查询end_date日价格是否满足pct得到list
+
+    stock_name = gd.get_stock_basic()
+    stock_name = stock_name[stock_name["name"].str.contains("ST") == False]
     stock_list = gd.get_daily(trade_date=end_date)
     # 红引线
     stock_list = stock_list[stock_list["open"]<stock_list["close"]]
     # stock_list = stock_list[stock_list["close"]>stock_list["pre_close"]]
     # 当天close>pre_close
-    stock_list = stock_list[stock_list["change"]>0]
+    # stock_list = stock_list[stock_list["change"]>0]
 
     # print(stock_list.shape)
     # print(stock_list.head())
-    stock_list = stock_list.eval('pct = (high-close)/(close-open)',inplace=False)
-
-    stock_list = stock_list[stock_list["pct"] >= pct][["ts_code"]]
-
+    # stock_list = stock_list.eval('pct = (high-close)/(close-open)',inplace=False)
+    stock_list = stock_list.eval('pct=(open-low)/(high-low)',inplace=False)
+    stock_list = stock_list[stock_list["pct"] >= pct]
     print(stock_list.shape)
+    stock_basic=gd.get_daily_basic(trade_date=end_date)
+    stock_basic.to_csv("bbb.csv")
+    stock_basic = stock_basic[(stock_basic["turnover_rate_f"]>2)&(stock_basic["turnover_rate"]>1)]
+    stock_basic = stock_basic[(stock_basic["total_mv"] < 2000000) | (stock_basic["circ_mv"] < 200000)]
+    stock_list = stock_list.merge(stock_basic,how="inner",on="ts_code")
+    stock_list = stock_list[stock_list["ts_code"].isin(stock_name["ts_code"])][["ts_code", "pct"]]
+    # stock_list.to_csv("f1.csv")
 
+    print(stock_basic.shape)
+    print(stock_list.shape)
 
 
     # date_list = gd.get_trade_date(start_date=start_date,end_date=end_date,is_open='1')['cal_date']
@@ -42,7 +54,7 @@ def f1(start_date = '20190920',end_date="20191018", period=15, times=12, pct=0.8
 
     i = 0
     for stock in stock_list['ts_code']:
-        df = gd.get_daily(ts_code=stock,start_date=start_date,end_date="20191017")
+        df = gd.get_daily(ts_code=stock,start_date=start_date,end_date=end_date)
         # t = df[df['change']>0]['ts_code'].count()
         df = df.eval('avg=amount/vol',inplace=False)
         df = df[["ts_code","trade_date","avg"]]
@@ -54,16 +66,17 @@ def f1(start_date = '20190920',end_date="20191018", period=15, times=12, pct=0.8
         ts = ts.drop(["index"], axis=1)
         df = pd.concat([df, ts], axis=1)
 
+
         t = df[df["avg"]-df["pre_avg"]>0]["ts_code"].count()
-        print(t)
+
         if t >= times:
             stocks.append(stock)
         i+=1
         if not i%180:
-            print("has run",i,'round')
+            print("has run", i, 'round')
             time.sleep(60)
 
-    pd.DataFrame(stocks).to_csv(end_date+'p'+str(period)+".csv")
+    pd.DataFrame(stocks).to_csv(end_date+'p'+str(period)+"up"+str(pct)+".csv")
     return stocks
 
 
@@ -119,8 +132,6 @@ def f2(start_date = '20190925',end_date="20191018", period=12, times=10, pct_low
 
     print(stock_list.shape)
 
-
-
     i = 0
     for stock in stock_list['ts_code']:
         df = gd.get_daily(ts_code=stock, start_date=start_date, end_date=end_date)
@@ -149,3 +160,5 @@ def f2(start_date = '20190925',end_date="20191018", period=12, times=10, pct_low
 
 def f3():
    pass
+
+f1()
--- model/model3.py
+++ model/model3.py
@@ -12,7 +12,7 @@ import data.get_data as gd
 # 需求2，12天，9天up,名字不包含st排序，第一up次数，第二涨幅,流通市值<10亿或者市值<200亿
 
 
-def f2(start_date='20190925', end_date="20191018", period=12, times=9, pct_low=0, pct_high=0.2):
+def f2(start_date='20190926', end_date="20191021", period=12, times=9, pct_low=0, pct_high=0.2):
     stocks = []
     # 获取股票代码list
     # if not end_date:
@@ -23,33 +23,28 @@ def f2(start_date='20190925', end_date="20191018", period=12, times=9, pct_low=0
     # print(end_date)
     # ['ts_code', 'trade_date', 'open', 'high', 'low', 'close', 'pre_close', 'change', 'pct_chg', 'vol', 'amount']
     # 查询end_date日价格是否满足pct得到list
+    # 不包含st的代码名
     stock_name = gd.get_stock_basic()
-    stock_name.to_csv("name.csv")
-    print(stock_name.shape)
     stock_name = stock_name[stock_name["name"].str.contains("ST") == False]
-    print(stock_name.shape)
-    stock_name.to_csv("name.csv")
-
+    # 当日市值满足条件的股票
     stock_mv = gd.get_daily_basic(trade_date=end_date, fields='ts_code,total_mv,circ_mv')
-    stock_mv.to_csv("mv.csv")
-    print(stock_mv.shape)
-    stock_mv = stock_mv[(stock_mv["total_mv"]<2000000) | (stock_mv["circ_mv"] < 100000)]
-    print(stock_mv.shape)
-
+    stock_mv = stock_mv[(stock_mv["total_mv"] < 2000000) | (stock_mv["circ_mv"] < 100000)]
+    # 第一天和最后一天的股票对比涨幅
     stock1 = gd.get_daily(trade_date="20190926")
     stock1 = stock1.eval("avg1=amount/vol", inplace=False)
     stock1 = stock1[["ts_code", "avg1"]]
-    print(stock1.shape)
 
     stock2 = gd.get_daily(trade_date=end_date)
     stock2 = stock2.eval("avg2=amount/vol", inplace=False)
     stock2 = stock2[["ts_code", "avg2"]]
-    print(stock2.shape)
+    # print(stock2.shape)
     stock1 = stock1.merge(stock2, how="inner", on="ts_code")
     stock1 = stock1.eval("pct=(avg2/avg1-1)", inplace=False)
+
     # 过滤出涨幅满足条件的
-    stock_list = stock1[(stock1["pct"] < 0.2) & (stock1["pct"] > 0.05)]
-    print(stock_list.shape)
+    stock_list = stock1[(stock1["pct"] < pct_high) & (stock1["pct"] > pct_low)]
+
+    # print(stock_list.shape)
     # 过滤掉市值不符的
     stock_list = stock_list[stock_list["ts_code"].isin(stock_mv["ts_code"])]
     print(stock_list.shape)
@@ -57,11 +52,7 @@ def f2(start_date='20190925', end_date="20191018", period=12, times=9, pct_low=0
     stock_list = stock_list[stock_list["ts_code"].isin(stock_name["ts_code"])][["ts_code", "pct"]]
     print(stock_list.shape)
 
-    # date_list = gd.get_trade_date(start_date=start_date,end_date=end_date,is_open='1')['cal_date']
-    # if date_list.count() < period:
-    #     return "period is wrong"
-    # for day in date_list:
-    #     print(day)
+
     i = 0
     for stock in stock_list['ts_code']:
         df = gd.get_daily(ts_code=stock, start_date=start_date, end_date=end_date)
@@ -77,6 +68,7 @@ def f2(start_date='20190925', end_date="20191018", period=12, times=9, pct_low=0
         df = pd.concat([df, ts], axis=1)
 
         t = df[df["avg"] - df["pre_avg"] > 0]["ts_code"].count()
+
         if t >= times:
             stocks.append([stock, t])
 
@@ -93,9 +85,10 @@ def f2(start_date='20190925', end_date="20191018", period=12, times=9, pct_low=0
     # res.columns = ["ts_code","up_times"]
     res = res.merge(stock_list, how="inner", on="ts_code")
     res = res.sort_values(["up_times", "pct"], ascending=[False,False])
+    res["pct"]= res["pct"].map(lambda x:('%.2f')%x)
+    print(res)
+    res.to_csv("sort"+end_date+"up"+str(times)+".csv")
 
-    # res.to_csv("sort"+end_date+"up"+str(times)+".csv")
-    res.set_option("precision", 4)
     print(res)
 
 
