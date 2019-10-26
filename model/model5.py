1d795abdb1ff2150901a5f1a25a5534963a9e01e
Jane <shaowenqin620@163.com>
2019-10-26 22:41:14 +0800

    model

--- model/model5.py
+++ model/model5.py
@@ -9,11 +9,14 @@ pro = ts.pro_api()
 # 满足给如一组基本信息，过滤
 # 给出一组指标，根据数据取值区间筛选，
 
+
 class StockFilter:
+
     # 默认过滤掉传入关键字，如果要
+
     def stock_basic(self, contain=False, **basic):
-        print(basic)
-        # basic = {'name':股票名，
+        # print(kwargs)
+        # basic = {'name':'股票名',
         #          'area': '所在地域',
         #          'industry': '所属行业',
         #          'market': '市场类型（主板/中小板/创业板/科创板）',
@@ -23,38 +26,51 @@ class StockFilter:
         #          'list_date': '上市日期',
         #          'delist_date': '退市日期',
         #          'is_hs': '是否沪深港通标的，N否 H沪股通 S深股通'}
-        stock_name = pro.stock_basic()
-        for key, value in basic.items():
-            print(key,value)
-            stock_name = stock_name[stock_name[key].str.contains(value) == contain]
 
-        return stock_name["ts_code"]
+        stock_basic = pro.stock_basic()
+        print(stock_basic.shape)
+
+        daily_basic = pro.daily_basic(trade_date="20191025")
+        print(daily_basic.shape)
+
+        for key, value in basic.items():
+            if key in list(stock_basic) :
+                stock_basic = stock_basic[stock_basic[key].str.contains(value) == contain]
+                print(stock_basic.shape)
+                basic[key] = ""
+            elif key in list(daily_basic):
+                daily_basic = daily_basic[(daily_basic[key] > value[0]) & (daily_basic[key] < value[1])]
+                basic[key] = ""
+                print(daily_basic.shape)
+
+        stock = pd.merge(stock_basic,daily_basic,on="ts_code")["ts_code"]
+
+        return stock
+
 
     # 每日指标
-    def stock_daily(self,**content):
-        content={"ts_code":"TS股票代码",
-                 "trade_date":"交易日期",
-                 "close":"当日收盘价",
-                 "turnover_rate":"换手率（ % ）",
-                 "turnover_rate_f":"换手率（自由流通股）",
-                 "volume_ratio": "量比",
-                 "pe": "市盈率（总市值 / 净利润）",
-                 "pe_ttm": "市盈率（TTM）",
-                 "pb": "市净率（总市值 / 净资产）",
-                 "ps": "市销率",
-                 "ps_ttm": "市销率（TTM）",
-                 "total_share": "总股本 （万股）",
-                 "float_share": "流通股本 （万股）",
-                 "free_share": "自由流通股本 （万）",
-                 "total_mv": "总市值 （万元）",
-                 "circ_mv": "流通市值（万元）",
-                 "": "",
-                 "": "",
-                 "": "",
-                 "": "",
-                 }
+    # def stock_daily(self,**content):
+    #     content={"ts_code":"TS股票代码",
+    #              "trade_date":"交易日期",
+    #              "close":"当日收盘价",
+    #              "turnover_rate":"换手率（ % ）",
+    #              "turnover_rate_f":"换手率（自由流通股）",
+    #              "volume_ratio": "量比",
+    #              "pe": "市盈率（总市值 / 净利润）",
+    #              "pe_ttm": "市盈率（TTM）",
+    #              "pb": "市净率（总市值 / 净资产）",
+    #              "ps": "市销率",
+    #              "ps_ttm": "市销率（TTM）",
+    #              "total_share": "总股本 （万股）",
+    #              "float_share": "流通股本 （万股）",
+    #              "free_share": "自由流通股本 （万）",
+    #              "total_mv": "总市值 （万元）",
+    #              "circ_mv": "流通市值（万元）",
+    #              "": "",
+    #              "": "",
+    #              "": "",
+    #              "": "",
+    #              }
 
 
-o = StockFilter()
-mm=o.stock_basic(market="科创板",contain=True)
-print(mm)
+
