883fb1e5783f2c3e8fdcbb6279f46840e73df167
Jane <shaowenqin620@163.com>
2020-03-05 22:49:00 +0800

    new

--- sql/data.py
+++ sql/data.py
@@ -17,7 +17,7 @@ tool = ts.pro.client.DataApi(token)
 engine = create_engine('postgresql://nezha:nezha@10.0.0.5:5432/stock', echo=False)
 
 
-def download_data(start_date=None, end_date=None, trade_date=None,days=3, tables=['daily', 'stk_limit', 'limit_list']):
+def download_data(start_date=None, end_date=None, trade_date=None, days=3, tables=['daily', 'stk_limit', 'limit_list']):
     if trade_date is None:
         if not end_date:
             end_date = str(datetime.datetime.today().date()).replace('-', '')
@@ -51,9 +51,9 @@ def download_data(start_date=None, end_date=None, trade_date=None,days=3, tables
 def read_data(table_name='daily', start_date='20200201', end_date=None, filter=True, **notcontain):
     # 基础数据
     # 科创板，st数据
-    if table_name in ['stock_basic','namechange']:
-        sql="select * from %s" % table_name
-        return pd.read_sql_query(sql,con=engine)
+    if table_name in ['stock_basic', 'namechange']:
+        sql = "select * from %s" % table_name
+        return pd.read_sql_query(sql, con=engine)
     if not end_date:
         end_date = str(datetime.datetime.today().date()).replace('-', '')
     sql = "select * from %s where (trade_date>='%s' and trade_date<='%s')" % (table_name, start_date, end_date)
@@ -63,8 +63,9 @@ def read_data(table_name='daily', start_date='20200201', end_date=None, filter=T
         NOTCONTAIN = sfilter(end_date, name="st|ST", market="科创板")
         data = data[data["ts_code"].isin(NOTCONTAIN['ts_code']) == False]
     return data
-def sfilter(self, trade_date=None, contain=True, **basic):
 
+
+def sfilter(self, trade_date=None, contain=True, **basic):
     # basic = {'name':'股票名',
     #          'area': '所在地域',
     #          'industry': '所属行业',
@@ -75,9 +76,9 @@ def sfilter(self, trade_date=None, contain=True, **basic):
     #          'list_date': '上市日期',
     #          'delist_date': '退市日期',
     #          'is_hs': '是否沪深港通标的，N否 H沪股通 S深股通'}
-    sql='select * from stock_basic'
+    sql = 'select * from stock_basic'
 
-    stock_basic=pd.read_sql('stock_basic',con=engine)
+    stock_basic = pd.read_sql('stock_basic', con=engine)
 
     res = pd.DataFrame()
     for key, value in basic.items():
@@ -90,7 +91,7 @@ def sfilter(self, trade_date=None, contain=True, **basic):
         #         daily_basic[key] = daily_basic[key] * 10000
         #     df = daily_basic[(daily_basic[key] > value[0]) & (daily_basic[key] < value[1])]
         #     basic[key] = ""
-            # print(daily_basic.shape)
+        # print(daily_basic.shape)
         # print("过滤股票条件%s" % key)
         res = res.append(df[["ts_code"]])
         # print(res.shape)
@@ -100,8 +101,8 @@ def sfilter(self, trade_date=None, contain=True, **basic):
     # return res["ts_code"]
     return res
 
-def save_data(data, filename, fp=None, fp_date=False,mode='w',header=True):
 
+def save_data(data, filename, fp=None, fp_date=False, mode='w', header=True):
     if not fp:
         fp = os.path.join(os.path.dirname(os.getcwd()), 'data', os.path.basename(sys.argv[0]).split('.py')[0])
 
@@ -111,21 +112,27 @@ def save_data(data, filename, fp=None, fp_date=False,mode='w',header=True):
         fp = os.path.join(fp, str(datetime.datetime.today().date()))
     if not os.path.exists(fp):
         os.makedirs(fp)
-    filename=os.path.join(fp, filename)
-    data.to_csv(filename,mode=mode,header=header)
+    filename = os.path.join(fp, filename)
+    data.to_csv(filename, mode=mode, header=header)
+
+
 def stock_basic():
-    df=pd.DataFrame()
+    df = pd.DataFrame()
     for status in list('LDP'):
-        df = pd.concat([tool.query('stock_basic',list_status=status,fields='ts_code,symbol,name,area,industry,market,list_date,list_status,delist_date'),df],ignore_index=True)
+        df = pd.concat([tool.query('stock_basic', list_status=status,
+                                   fields='ts_code,symbol,name,area,industry,market,list_date,list_status,delist_date'),
+                        df], ignore_index=True)
         # print(df.shape)
-    df.to_sql('stock_basic', con=engine, if_exists='replace' ,index=False)
+    df.to_sql('stock_basic', con=engine, if_exists='replace', index=False)
     print('stock_basic has done')
+
+
 # def cal(**kwargs):
 #     tool.query()
-def cal(start=None,end=None,days=3):
+def cal(start=None, end=None, days=3):
     if not end:
-        end= str(datetime.datetime.today().date()).replace('-', '')
-    cal_list=tool.query('trade_cal', start_date=start, end_date=end, is_open='1')
+        end = str(datetime.datetime.today().date()).replace('-', '')
+    cal_list = tool.query('trade_cal', start_date=start, end_date=end, is_open='1')
     if start:
         return cal_list['cal_date']
 
@@ -136,18 +143,26 @@ def cal(start=None,end=None,days=3):
 def adj_share():
     pass
 
+
 def new_list():
-    ll=read_data('stock_basic')
-    ll=ll.loc[ll['list_date']>='20200101']
-    df=read_data('daily',start_date='20200101').merge(ll[['ts_code','list_date']],left_on=['ts_code','trade_date'],right_on=['ts_code','list_date'])
-    df1 = read_data('limit_list', start_date='20200101').merge(ll[['ts_code', 'list_date']],
-                                                         on='ts_code')
-    df2 = read_data('stk_limit', start_date='20200101').merge(ll[['ts_code', 'list_date']],
-                                                         on='ts_code')
+    start='20170101'
+    ll = read_data('stock_basic')
+    # ll = ll.loc[ll['list_date'] >= '20200101']
+    df = read_data('daily', start_date=start).merge(ll[['ts_code', 'list_date']],
+                                                         left_on=['ts_code', 'trade_date'],
+                                                         right_on=['ts_code', 'list_date'])
+    print(df.shape,df[df['high']==df['low']].shape,df[df['high']==df['close']].shape,df[(df['high']==df['close']) &( df['high']>df['low'])].shape)
+    print(df['trade_date'].min())
+    df1 = read_data('limit_list', start_date=start).merge(ll[['ts_code', 'list_date']],
+                                                               on='ts_code')
+    df2 = read_data('stk_limit', start_date=start).merge(ll[['ts_code', 'list_date']],
+                                                              on='ts_code')
     print()
+
 
 if __name__ == '__main__':
     new_list()
+
     # download_data()
     # if datetime.datetime.today().weekday()%2==0:
     #     stock_basic()
