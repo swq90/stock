257e05e236ea75094b75430a95b29a130722d6fb
Jane <shaowenqin620@163.com>
2020-05-24 22:34:32 +0800

    pct*preclose <=high or close

--- subject/continue_limit.py
+++ subject/continue_limit.py
@@ -8,57 +8,7 @@ from stock.util.basic import basic
 days=2
 
 # 连续两天一字板，或一字板-一字板回封
-def red_line(data):
-    # raw_data = read_data('daily', start_date=start_date, end_date=end_date)
-    # limit = read_data('stk_limit', start_date=start_date, end_date=end_date)
-    # data = raw_data.merge(limit[['ts_code', 'trade_date', 'up_limit']], on=['ts_code', 'trade_date'])
-    data['red_line']=data.apply(lambda x: 1 if ((x['up_limit']==x['close'])&(x['low']==x['close'])) else 0,axis=1)
-    data['reback_limit']=data.apply(lambda x: 1 if ((x['up_limit']==x['close'])&(x['open']==x['close'])) else 0,axis=1)
-    pre = basic().pre_data(data, label=['red_line'], pre_days=1)
-    data = data.merge(pre[['ts_code', 'trade_date', 'pre_1_red_line']], on=['ts_code', 'trade_date'])
-    data=data.loc[(data['pre_1_red_line'] == 1)&(data['reback_limit'] == 1)]
-    print(data.shape)
-    list_days=basic().list_days(data)
-    print(list_days.shape)
-    data=data.iloc[:,:2].merge(list_days,on=['ts_code','trade_date'])
-    print(data.shape)
-    return data
-# 一字板-一字板，一字板-一字板回封
-# 如果买入当天涨停则不处理
-def roi(data,raw_data):
-    summary=pd.DataFrame()
-    print(data.shape[0])
-    all_data = raw_data.copy()
-    for pctb in range(-10,11,2):
 
-        if pctb==-10:
-            all_data['price_buy']=all_data['down_limit']
-        elif pctb==10:
-            all_data['price_buy']=all_data['up_limit']
-        else:
-
-            all_data['price_buy']=all_data['pre_close']*(1+0.01*pctb)
-
-        for pcts in range(-10,11,2):
-            if pcts == -10:
-                all_data['price_sell'] = all_data['down_limit']
-            elif pcts == 10:
-                all_data['price_sell'] = all_data['up_limit']
-            else:
-                all_data['price_sell'] = all_data['pre_close']*(1+0.01*pctb)
-            all_data=all_data.loc[(all_data['price_buy']>=all_data['low'])&(all_data['price_sell']<=all_data['high'])]
-
-            res=sheep.wool2(data,all_data,PRICEB='price_buy',PRICES='price_sell')
-            detail=pd.DataFrame({'buy':{pctb},'sell':{pcts},'day':{res.shape[0]},'n':{res['n'].sum()},'pct_avg':{res['pct'].mean()},'pct_all':{res.iloc[-1,-1]}})
-            summary=pd.concat([summary,detail],ignore_index=True)
-            print()
-
-
-
-
-
-
-print()
 
 def process(data,condions):
     '''
@@ -87,51 +37,44 @@ def filter(data,conditions):
             pre = basic().pre_data(data, label=[conditions[day]], pre_days=day)
             res = res.merge(pre[['ts_code', 'trade_date', 'pre_%s_%s'%(day,conditions[day])]], on=['ts_code', 'trade_date'])
             res = res.loc[res['pre_%s_%s'%(day,conditions[day])] == 1]
+            data=data.loc[data['ts_code'].isin(res['ts_code'].unique())]
+
             print(res.shape,data.shape)
     return res
 
-# def buy_data(data,PB):
-#     if PB in data.columns:
-#         return data
-#     return
-#
-# def sell_data(data,PS):
-#     if PS in data.columns:
-#         return data
-#     return
-#
-#
-# def sales_model(model,):
-#     M=['cut']#买卖模式，cut代表买入切分
-#     # 对应每个回测方法有不同的pb，ps
 
-def roi(red_line,raw_data,mode='PB'):
+
+def roi(red_line,raw_data):
     res=[]
     section=['down_limit']+list(range(-8,10,2))+['up_limit']
     red_line['low_pct']=100*(red_line['low']/red_line['pre_close']-1)
 
     for pb in section:
         if pb not in raw_data.columns:
-            if type(pb)=='int':
+            if isinstance(pb,int):
                 PB = 'PB'
                 red_line[PB]=red_line['pre_close']* (1 + 0.01 * pb)
                 raw_data[PB] = raw_data['pre_close'] * (1 + 0.01 * pb)
-            else:
-                PB=pb
+        else:
+            PB=pb
+        grass = red_line.loc[red_line[PB] >= red_line['low']]
+        if grass.empty:
+            continue
         for ps in section:
             if ps not in raw_data.columns:
-                if type(ps) == 'int':
+                if isinstance(ps,int):
                     PS = 'PS'
                     raw_data[PS] = raw_data['pre_close'] * (1 + 0.01 * ps)
-                else:
-                    PS=ps
+            else:
+                PS=ps
 
-            raw_data[PS]=raw_data.apply(lambda x:x[ps] if x[ps]<x['high'] else None)
+            raw_data[PS]=raw_data.apply(lambda x:x[PS] if x[PS]<=x['high'] else x['close'],axis=1)
 
-            meat=sheep.wool2(red_line.loc[red_line[PB]>=red_line['low']],raw_data,PRICEB=PB,PRICES=PS)
-
+            meat=sheep.wool2(grass,raw_data,PRICEB=PB,PRICES=PS).dropna()
+            if meat.empty:
+                continue
             res.append([pb,ps,meat.shape[0],meat.iloc[-1,-1]])
-        res=pd.DataFrame(res,columns=['pb','ps','n_days','pct'])
+    res=pd.DataFrame(res,columns=['pb','ps','n_days','pct'])
     return res
 
 
@@ -139,18 +82,20 @@ def roi(red_line,raw_data,mode='PB'):
 if __name__=='__main__':
     start_date='20%s0101'
     end_date='20%s1231'
-    for year in range(19,21):
+    for year in range(18,21):
         raw_data = read_data('daily', start_date=start_date%year, end_date=end_date%year).iloc[:,:-2]
+
         limit = read_data('stk_limit', start_date=start_date%year, end_date=end_date%year)
         raw_data = raw_data.merge(limit[['ts_code', 'trade_date', 'up_limit','down_limit']], on=['ts_code', 'trade_date'])
-
-        print(start_date%year,'----',end_date%year,'include %s items'%raw_data.shape[0])
         list_days = basic().list_days(raw_data,list_days=25)
         print(list_days.shape)
         raw_data = raw_data.merge(list_days, on=['ts_code', 'trade_date'])
+        print(start_date%year,'----',end_date%year,'include %s items'%raw_data.shape[0])
+
         raw_data=process(raw_data,[ 'ULRL','ULL'])
         red_line=filter(raw_data,[0,'ULRL','ULL'])
-        save_data(red_line,'new%s连板.csv'%year)
+
+        save_data(red_line,'new%s连板d.csv'%year)
         res=roi(red_line,raw_data)
-        save_data(res,'%s连板回溯.csv'%year)
+        save_data(res,'%s连板回溯d.csv'%year)
         # res=yunei(start_date%year,end_date%year)
